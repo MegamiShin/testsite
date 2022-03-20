58a869204f921f2657027ce57cd4e6d43c976769
Shin Megami Tensei <82964945+MegamiShin@users.noreply.github.com>
2022-03-20 18:51:51 +0100

    yes

---
+++ components/Icons/Logo.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="h-12 w-12">
+    <nuxt-link
+      to="/"
+      class="text-blue-500 dark:text-cyan-400 transform scale-90"
+    >
+<svg width="48" height="56" xmlns="http://www.w3.org/2000/svg" desc="Created with imagetracer.js version 1.2.6" version="1.1">
+
+ <g>
+  <title>Layer 1</title>
+  <path id="svg_1" d="m21.5,6l5,0l17.5,10.5l1,2l0,21l-1,2.5l-17.5,10l-5,0l-17.5,-10l-1,-2.5l0,-21l1,-2.5l17.5,-10zm1.5,2l-16,9l-2,2l0,21l18,10l3,0l16,-9l1,-1l0,-21l-15,-10l-5,-1z" opacity="0.79216" stroke="rgb(29,174,199)" fill="rgb(29,174,199)"/>
+  <path id="svg_2" d="m10,19l4,1.5l9.5,12.5q2.3,0.8 1.5,-1.5l0.5,-2.5l2.5,1l7.5,-11l2.5,0.5l0,18.5l-2,-0.5l0,-14l-2,1l-6,8l2,4l-2,-1l-0.5,-1.5l-2.5,4q-2.7,1.1 -2,-1.5l-10.5,-13.5l-0.5,15l-2,-0.5l0,-18.5z" opacity="0.79216" stroke="rgb(29,174,199)" fill="rgb(29,174,199)"/>
+  <path id="svg_3" d="m16,19l3,1.5l-3,0.5l0,-2z" opacity="0.79216" stroke="rgb(29,174,199)" fill="rgb(29,174,199)"/>
+  <path id="svg_4" d="m16,32l2,0l0,6l-2,0l0,-6z" opacity="0.79216" stroke="rgb(29,174,199)" fill="rgb(29,174,199)"/>
+  <path id="svg_5" d="m0,0l48,0l0,56l-48,0l0,-56zm22,6l-18,10l-1,3l0,21l1,2l18,10l5,0l17,-10l1,-2l0,-21l-1,-2l-17,-11l-5,0z" opacity="0" stroke="rgb(0,0,0)" fill="rgb(0,0,0)"/>
+  <path id="svg_6" d="m21.16667,9.16667l5,1l15.5,9.5l0,21l-1.5,1.5l-16,9l-3,0l-17.5,-10l0,-21.5l1.5,-1.5l16,-9zm-12.5,11l0,19l2,0l1,-15l10,14q-1,2 2,1l3,-4l0,2l2,1l-2,-4l6,-8l2,-1l0,14l2,0l0,-18l-2,-1l-8,11l-3,2q1,2 -1,1l-10,-12l-4,-2zm6,0l0,2l3,0l-3,-2zm0,13l0,6l2,0l0,-6l-2,0z" opacity="0" stroke="rgb(0,0,0)" fill="rgb(0,0,0)"/>
+  <rect stroke-width="0" id="svg_7" height="7.83333" width="4.33333" y="31.05583" x="13.66667" stroke="#000" fill="#fff"/>
+  <rect stroke="#000" stroke-width="0" id="svg_8" height="7" width="2" y="31.7225" x="17" fill="#fff"/>
+  <ellipse ry="2.75" rx="4.33333" id="svg_9" cy="19.63917" cx="18.66667" stroke-width="0" stroke="#000" fill="#fff"/>
+  <ellipse ry="2.66667" rx="2.41667" id="svg_11" cy="36.05583" cx="29.75" stroke-width="0" stroke="#000" fill="#fff"/>
+  <ellipse stroke-dasharray="2,2" ry="3.08333" rx="1.91667" id="svg_13" cy="29.63917" cx="24.91667" stroke-width="0" stroke="rgb(0,0,0)" fill="#ffffff"/>
+  <rect id="svg_14" height="1.16667" width="0.66667" y="28.2225" x="27" stroke-dasharray="2,2" stroke-width="0" stroke="rgb(0,0,0)" fill="#ffffff"/>
+  <rect id="svg_15" height="0.33333" width="1" y="23.38917" x="83.16667" stroke-dasharray="2,2" stroke-width="0" stroke="rgb(0,0,0)" fill="#ffffff"/>
+  <rect stroke="rgb(0,0,0)" id="svg_16" height="3" width="6.50002" y="36.88917" x="21.16669" stroke-dasharray="2,2" stroke-width="0" fill="#ffffff"/>
+  <rect stroke-width="0" id="svg_18" height="2.16667" width="1.83333" y="28.05583" x="25.33333" stroke="rgb(0,0,0)" fill="#ffffff"/>
+  <ellipse stroke-width="0" ry="2" rx="0.58333" id="svg_19" cy="34.05583" cx="28.75" stroke="rgb(0,0,0)" fill="#ffffff"/>
+ </g>
+</svg>
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Logo'
+};
+</script>
---
+++ components/Navigation/SocialHandles.vue
@@ -0,0 +1,83 @@
+<template>
+  <article
+    class="hidden text-gray-500 dark:text-gray-400 lg:block fixed left-8 bottom-0 z-10"
+  >
+    <ul class="p-4">
+      <li>
+        <a
+          href="https://github.com/GypsyTheDj"
+          target="_blank"
+          rel="noreferrer"
+          class="mb-2 h-8 w-8 grid place-items-center hover:text-blue-500 focus:text-blue-500 dark:hover:text-cyan-400 dark:focus:text-cyan-400 rounded-full transform transition duration-300 hover:-translate-y-1 focus:-translate-y-1 focus:outline-none"
+        >
+          <git-hub class="h-2/3 w-2/3" />
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://twitter.com/Thisispappi254"
+          target="_blank"
+          rel="noreferrer"
+          class="mb-2 h-8 w-8 grid place-items-center hover:text-blue-500 focus:text-blue-500 dark:hover:text-cyan-400 dark:focus:text-cyan-400 rounded-full transform transition duration-300 hover:-translate-y-1 focus:-translate-y-1 focus:outline-none"
+        >
+          <twitter class="h-2/3 w-2/3" />
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://www.linkedin.com/in/normannuthu/"
+          target="_blank"
+          rel="noreferrer"
+          class="mb-2 h-8 w-8 grid place-items-center hover:text-blue-500 focus:text-blue-500 dark:hover:text-cyan-400 dark:focus:text-cyan-400 rounded-full transform transition duration-300 hover:-translate-y-1 focus:-translate-y-1 focus:outline-none"
+        >
+          <linked-in class="h-2/3 w-2/3" />
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://www.facebook.com/TeamSuccessMusicLtd"
+          target="_blank"
+          rel="noreferrer"
+          class="mb-2 h-8 w-8 grid place-items-center hover:text-blue-500 focus:text-blue-500 dark:hover:text-cyan-400 dark:focus:text-cyan-400 rounded-full transform transition duration-300 hover:-translate-y-1 focus:-translate-y-1 focus:outline-none"
+        >
+          <facebook class="h-2/3 w-2/3" />
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://medium.com/@normannuthu"
+          target="_blank"
+          rel="noreferrer"
+          class="mb-2 h-8 w-8 grid place-items-center hover:text-blue-500 focus:text-blue-500 dark:hover:text-cyan-400 dark:focus:text-cyan-400 rounded-full transform transition duration-300 hover:-translate-y-1 focus:-translate-y-1 focus:outline-none"
+        >
+          <medium class="h-2/3 w-2/3" />
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://youtube.com/c/GypsyTheDj"
+          target="_blank"
+          rel="noreferrer"
+          class="mb-2 h-8 w-8 grid place-items-center hover:text-blue-500 focus:text-blue-500 dark:hover:text-cyan-400 dark:focus:text-cyan-400 rounded-full transform transition duration-300 hover:-translate-y-1 focus:-translate-y-1 focus:outline-none"
+        >
+          <you-tube class="h-2/3 w-2/3" />
+        </a>
+      </li>
+    </ul>
+    <div class="mx-auto h-24 w-px bg-gray-400 dark:bg-gray-500"></div>
+  </article>
+</template>
+
+<script>
+import Facebook from '../Icons/Facebook.vue';
+import GitHub from '../Icons/GitHub.vue';
+import LinkedIn from '../Icons/LinkedIn.vue';
+import Medium from '../Icons/Medium.vue';
+import Twitter from '../Icons/Twitter.vue';
+import YouTube from '../Icons/YouTube.vue';
+
+export default {
+  name: 'SocialHandles',
+  components: { GitHub, Twitter, LinkedIn, Facebook, Medium, YouTube }
+};
+</script>
